6b4d9342c690e653968041f271a18381d4b49e64
sahil221d <sahil.dabake@gmail.com>
2022-07-08 23:58:03 +0530

    final commit

--- basicbanking/basicbanking/settings.py
+++ basicbanking/basicbanking/settings.py
@@ -13,6 +13,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = ["bscbank.herokuapp.com", "localhost"]
+CSRF_TRUSTED_ORIGINS = ['https://bscbank.herokuapp.com/']
+
 
 
 # Application definition
